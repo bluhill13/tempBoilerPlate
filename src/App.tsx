6fc33216f0b510f75dc1bd1dd6a2b6d0ef042888
Eivind Skrede <eivindls@hotmail.com>
2022-11-11 11:35:47 +0100

    Swithced to redux-typescript template

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import LocaleContext from './context/LocaleContext';
+import AuthContext from './context/AuthContext';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useEffect, useState } from 'react';
+import MainLayout from './layout/MainLayout';
+import './App.css';
+
+function App() {
+	const [locale] = useState('nb');
+
+	useEffect(() => {
+		document.title = 'Adecco Client Portal';
+	}, []);
+
+	console.log('App', locale);
+
+	return (
+		<div>
+			øø
+			<LocaleContext.Provider value={locale}>
+				<MainLayout />
+				hge
+				<ToastContainer position="bottom-right" />
+			</LocaleContext.Provider>
+		</div>
+	);
+}
+
+export default App;
